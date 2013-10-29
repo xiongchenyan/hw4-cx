df360e75cf9a4b5662f44b1f52b8b40529ec411b
xiongchenyan <xiongchenyan@gmail.com>
2013-10-28 20:02:50 -0400

    implementation finished.

--- src/main/java/edu/cmu/lti/f13/hw4/hw4_cx/annotators/DocumentVectorAnnotator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_cx/annotators/DocumentVectorAnnotator.java
@@ -1,14 +1,28 @@
 package edu.cmu.lti.f13.hw4.hw4_cx.annotators;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Properties;
+
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
+import org.apache.uima.fit.util.FSCollectionFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.IntegerArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.fit.util.FSCollectionFactory;
 
 import edu.cmu.lti.f13.hw4.hw4_cx.typesystems.Document;
+import edu.cmu.lti.f13.hw4.hw4_cx.typesystems.Token;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
@@ -29,13 +43,45 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 	 * @param doc
 	 */
 
+	//first tokenize the document, and then update the doc->TokenList
 	private void createTermFreqVector(JCas jcas, Document doc) {
 
 		String docText = doc.getText();
 		
 		//TO DO: construct a vector of tokens and update the tokenList in CAS
+		Properties props = new Properties();
+		props.put("annotators", "tokenize");
 		
-
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		
+		
+		edu.stanford.nlp.pipeline.Annotation sentence = new edu.stanford.nlp.pipeline.Annotation(docText);
+		pipeline.annotate(sentence);
+		
+		HashMap<String, Integer> hToken = new HashMap<String, Integer>(); //called stanford nlp to tokenize the document
+		
+		for (CoreLabel token : sentence.get(TokensAnnotation.class))
+		{
+			String word = token.word().toLowerCase();
+			
+			if (!hToken.containsKey(word))
+			{
+				hToken.put(word,1);				
+			}
+			hToken.put(word, hToken.get(word) + 1);
+		}
+		//hToken now contains all token counts
+		
+		List<Token> TokenInDoc = new ArrayList<Token>();
+		for (Entry<String, Integer> item : hToken.entrySet())
+		{
+			Token token = new Token(jcas);
+			token.setFrequency(item.getValue());
+			token.setText(item.getKey());
+			token.addToIndexes(); //add tokens to jcas
+			TokenInDoc.add(token);				
+		}
+		doc.setTokenList(FSCollectionFactory.createFSList(jcas,TokenInDoc));
 	}
 
 }
--- src/main/java/edu/cmu/lti/f13/hw4/hw4_cx/casconsumers/RetrievalEvaluator.java
+++ src/main/java/edu/cmu/lti/f13/hw4/hw4_cx/casconsumers/RetrievalEvaluator.java
@@ -2,7 +2,11 @@ package edu.cmu.lti.f13.hw4.hw4_cx.casconsumers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
@@ -15,23 +19,31 @@ import org.apache.uima.resource.ResourceProcessException;
 import org.apache.uima.util.ProcessTrace;
 
 import edu.cmu.lti.f13.hw4.hw4_cx.typesystems.Document;
+import edu.cmu.lti.f13.hw4.hw4_cx.typesystems.Token;
+import edu.cmu.lti.f13.hw4.hw4_cx.utils.Utils;
 
 
 public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 	/** query id number **/
 	public ArrayList<Integer> qIdList;
-
 	/** query and text relevant values **/
 	public ArrayList<Integer> relList;
-
+	public ArrayList<Double> scoreList;
+	public ArrayList<Map<String,Integer>> hVectorList;
+	public ArrayList<String> vRawString;
+	public ArrayList<Integer> vRank;
+	public Map<Double,Integer> hScoreRank;
 		
 	public void initialize() throws ResourceInitializationException {
 
 		qIdList = new ArrayList<Integer>();
-
 		relList = new ArrayList<Integer>();
-
+		scoreList = new ArrayList<Double>();
+		hVectorList = new ArrayList<Map<String,Integer>>();
+		vRawString = new ArrayList<String>();
+		vRank = new ArrayList<Integer> ();	
+		hScoreRank = new HashMap<Double,Integer>();
 	}
 
 	/**
@@ -54,18 +66,27 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 			Document doc = (Document) it.next();
 
 			//Make sure that your previous annotators have populated this in CAS
-			FSList fsTokenList = doc.getTokenList();
-			//ArrayList<Token>tokenList=Utils.fromFSListToCollection(fsTokenList, Token.class);
 
 			qIdList.add(doc.getQueryID());
 			relList.add(doc.getRelevanceValue());
-			
-			//Do something useful here
-
+			List<Token> TokenInDoc = Utils.fromFSListToCollection(doc.getTokenList(),Token.class);
+			Map<String,Integer> hVector = MakeMapFromList(TokenInDoc);
+			hVectorList.add(hVector);
+			String RawText = doc.getText();
+			vRawString.add(RawText);	
 		}
 
 	}
-
+	
+	private Map<String,Integer> MakeMapFromList(List<Token> vToken)
+	{
+		Map<String,Integer> hVector = new HashMap<String,Integer>();
+		for (Token token : vToken)
+		{
+			hVector.put(token.getText(), token.getFrequency());
+		}
+		return hVector;
+	}
 	/**
 	 * TODO 1. Compute Cosine Similarity and rank the retrieved sentences 2.
 	 * Compute the MRR metric
@@ -76,18 +97,30 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 
 		super.collectionProcessComplete(arg0);
 
-		// TODO :: compute the cosine similarity measure
-		
-		
-		
-		// TODO :: compute the rank of retrieved sentences
-		
-		
-		
-		// TODO :: compute the metric:: mean reciprocal rank
+		//compute the cosine similarity measure
+		AssignScore();
+		//compute the rank of retrieved sentences
+		CalcRank();
+		//compute the metric:: mean reciprocal rank
 		double metric_mrr = compute_mrr();
+		
+		//show result
+		ShowResult();
+		
 		System.out.println(" (MRR) Mean Reciprocal Rank ::" + metric_mrr);
 	}
+	
+	/**
+	 * output result to stdout
+	 */
+	private void ShowResult()
+	{
+		for (int i = 0; i < qIdList.size(); i ++)
+		{
+			System.out.format("Score: %f\trank=%d\trel=%d\tqid=%d\t%s\n", scoreList.get(i),vRank.get(i),relList.get(i),qIdList.get(i),vRawString.get(i));
+		}
+	}
+	
 
 	/**
 	 * 
@@ -98,11 +131,43 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		double cosine_similarity=0.0;
 
 		// TODO :: compute cosine similarity between two sentences
-		
-
+		double NormQ = NormOfVector(queryVector);
+		double NormD = NormOfVector(docVector);
+		if (NormQ == 0 || NormD == 0)
+		{
+			return 0;
+		}
+		cosine_similarity = InnerProduct(queryVector,docVector) / (NormQ * NormD);
 		return cosine_similarity;
 	}
-
+	
+	private double NormOfVector(Map<String,Integer> hVector)
+	{
+		double Norm = 0;
+		for (Entry<String,Integer> item : hVector.entrySet())
+		{
+			Integer value = item.getValue();
+			Norm += value * value;
+		}
+		Norm = Math.sqrt(Norm);
+		return Norm;		
+	}
+	private double InnerProduct(Map<String,Integer> hA, Map<String,Integer> hB)
+	{
+		double res = 0;
+		for (Entry<String,Integer> item : hA.entrySet())
+		{
+			String key = item.getKey();
+			Integer value = item.getValue();
+			if (hB.containsKey(key))
+			{
+				res += hB.get(key) * value;				
+			}					
+		}		
+		return res;
+	}
+	
+	
 	/**
 	 * 
 	 * @return mrr
@@ -111,8 +176,93 @@ public class RetrievalEvaluator extends CasConsumer_ImplBase {
 		double metric_mrr=0.0;
 
 		// TODO :: compute Mean Reciprocal Rank (MRR) of the text collection
-		
+		//only need the relList and vRank
+		int cnt = 0;
+		for (int i = 0; i < relList.size(); i ++)
+		{
+			if (relList.get(i) == 1)
+			{
+				metric_mrr += 1.0 / vRank.get(i);
+				cnt += 1;
+			}			
+		}
+		metric_mrr /= cnt;
 		return metric_mrr;
+	}
+	/**
+	 * calc the cosine score of each document.
+	 * document is in  hVectorList, if relList.get(i) is 99, then it is the current query.
+	 * assume the query appears first
+	 * @return
+	 */	
+	private Boolean AssignScore()
+	{
+		Map<String,Integer> vCurrentQ = new HashMap<String,Integer>();
+		for (int i = 0; i < relList.size(); i ++)
+		{
+			if (relList.get(i) == 99)
+			{
+				vCurrentQ = hVectorList.get(i);
+				scoreList.add(0.0);
+				continue;
+			}
+			Double score = computeCosineSimilarity(vCurrentQ,hVectorList.get(i));
+			scoreList.add(score);
+		}		
+		return true;
+	}
+	/**
+	 * calc the rank of each doc, based on their score, and qIdList.
+	 * how to:
+	 * 	make a qid-score to rank hashmap
+	 * 		for each qid, get all its score, sort them, and then add to qid
+	 * @return
+	 */
+	private Boolean CalcRank()
+	{
+		ArrayList<Double> vCurrentScore = new ArrayList<Double>();
+		for (int i = 0; i < scoreList.size(); i ++)
+		{
+			if (i != 0)
+			{
+				if (qIdList.get(i) != qIdList.get(i - 1))
+				{
+					MakeQidScoreHash(vCurrentScore, qIdList.get(i - 1));
+					vCurrentScore.clear();
+				}
+			}
+			vCurrentScore.add(scoreList.get(i));
+		}
+		MakeQidScoreHash(vCurrentScore,qIdList.get(qIdList.size() - 1));
+		vCurrentScore.clear();
+		//now the qid.score -> rank mapping is done
+		vRank.clear();
+		for (int i = 0; i < scoreList.size(); i ++)
+		{
+			Double key = qIdList.get(i) * 10 + scoreList.get(i);
+			Integer rank = hScoreRank.get(key);
+			vRank.add(rank);
+		}
+		return true;
+	}
+	
+	/**
+	 * 
+	 * @param vScore the score for this qid
+	 * @param qid the id of query
+	 * @return a map from Qid.Score to rank
+	 */
+	private void MakeQidScoreHash(ArrayList<Double> vScore, Integer qid)
+	{
+		Collections.sort(vScore,Collections.reverseOrder());
+		for (int i = 0; i < vScore.size(); i ++)
+		{
+			Double key = qid * 10 + vScore.get(i);
+			if (!hScoreRank.containsKey(key))
+			{
+				hScoreRank.put(key, i + 1);
+			}			
+		}	
 	}
 
 }
